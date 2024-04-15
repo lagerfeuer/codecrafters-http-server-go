be3cf4e22853d437191fcfbd0e6d4383cd3ea9a7
Lukas Deutz <github@deutz.io>
2024-04-15 17:39:15 -0400

    Respond with content

--- app/server.go
+++ app/server.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var (
 	httpVersion = "HTTP/1.1"
-	endOfLine   = "\r\n\r\n"
+	endOfLine   = "\r\n"
 
 	httpStatusCodes = map[int]string{
 		200: "OK",
@@ -17,9 +18,50 @@ var (
 	}
 )
 
-func makeResponse(statusCode int) []byte {
-	s := fmt.Sprintf("%s %d %s %s", httpVersion, statusCode, httpStatusCodes[statusCode], endOfLine)
-	return []byte(s)
+// unused
+type Request struct {
+	method  string
+	uri     string
+	version string
+	headers map[string]string
+}
+
+type Response struct {
+	statusCode int
+	headers    map[string]string
+	body       string
+}
+
+func NewResponse() *Response {
+	return &Response{
+		statusCode: 404,
+		headers:    map[string]string{},
+		body:       "",
+	}
+}
+
+func (r *Response) ToBytes() []byte {
+	var sb strings.Builder
+	sb.WriteString(
+		fmt.Sprintf("%s %d %s%s", httpVersion, r.statusCode, httpStatusCodes[r.statusCode], endOfLine),
+	)
+
+	body := []byte(r.body)
+	if r.body != "" {
+		r.headers["Content-Type"] = "text/plain"
+		r.headers["Content-Length"] = strconv.Itoa(len(body))
+	}
+
+	for key, val := range r.headers {
+		sb.WriteString(fmt.Sprintf("%s: %s%s", key, val, endOfLine))
+	}
+	sb.WriteString(endOfLine)
+
+	if r.body != "" {
+		sb.WriteString(r.body)
+	}
+
+	return []byte(sb.String())
 }
 
 func handle(conn net.Conn) error {
@@ -32,17 +74,21 @@ func handle(conn net.Conn) error {
 		return err
 	}
 
-	response := makeResponse(404)
+	response := NewResponse()
 
 	content := string(buffer)
 	headers := strings.Split(content, endOfLine)
 	startLine := strings.Split(headers[0], " ")
 
 	if startLine[1] == "/" {
-		response = makeResponse(200)
+		response.statusCode = 200
+	}
+	if strings.HasPrefix(startLine[1], "/echo/") {
+		response.statusCode = 200
+		response.body = startLine[1][len("/echo/"):]
 	}
 
-	_, err = conn.Write(response)
+	_, err = conn.Write(response.ToBytes())
 	if err != nil {
 		fmt.Println("Error writing to connection: ", err.Error())
 		return err
